df458c3c062e65073363e8ccf14f32cadd5e6292
Mati20041 <mati20041@github>
2021-05-03 23:15:12 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.png';
+import './App.css';
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>Docker Workshops Sample React App</h1>
+                <img src={logo} className="App-logo" alt="logo"/>
+                <div style={{marginTop: '16rem'}}>Icons made by <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a
+                    href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
+            </header>
+        </div>
+    );
+}
+
+export default App;
